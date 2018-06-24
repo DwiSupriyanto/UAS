8ed9a24579a4858878a251c999ac6535fcf8c596
DwiSupriyanto <dwisupriyanto181298@gmail.com>
2018-06-25 01:02:38 +0700

    project uas

---
+++ app/src/main/java/dwi/budiluhur/com/uassms1/MessageReceiver.java
@@ -0,0 +1,39 @@
+package dwi.budiluhur.com.uassms1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.gsm.SmsMessage;
+import android.widget.Toast;
+
+
+public class MessageReceiver extends BroadcastReceiver {
+    @Override    public void onReceive(Context context, Intent intent) {
+         Bundle bundle = intent.getExtras();
+        SmsMessage[] messages;
+        String str = "";
+
+        if (bundle != null) {
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            messages = new SmsMessage[pdus != null ? pdus.length : 0];
+            for (int i=0; i<messages.length; i++) {
+                messages[i] = SmsMessage.createFromPdu((byte[]) (pdus != null ? pdus[i] : null));
+                str += messages[i].getOriginatingAddress();
+                str += ": ";
+                str += messages[i].getMessageBody();
+                str += "\n";
+            }
+
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+
+             Intent broadcastIntent = new Intent();
+            broadcastIntent.setAction("SMS_RECEIVED_ACTION");
+            broadcastIntent.putExtra("messages", str);
+            context.sendBroadcast(broadcastIntent);
+        }
+
+    }
+	
+
+}
